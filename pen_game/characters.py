a04721ed646d8a96f81c9438905f2556ba227639
marisamak <marinasamakova1507@gmail.com>
2025-02-07 23:13:28 +0300

    add

--- pen_game/characters.py
+++ pen_game/characters.py
@@ -3,8 +3,10 @@ import random
 
 def different_circles(screen):
     for i in range(10):
-        x = random.randint(0,640)
-        y = random.randint(0,480)
+        width, height = screen.get_size()
+
+        x = random.randint(0,width)
+        y = random.randint(0,height)
         radius = random.randint(10, 100)
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         pygame.draw.circle(screen, color, (x, y), radius)
