12e71e6f26b0b22cc8c3f03d11dcd50a8d261200
marisamak <marinasamakova1507@gmail.com>
2025-02-18 15:26:18 +0300

    +

--- rects_in_conners/rects_in_conners.py
+++ rects_in_conners/rects_in_conners.py
@@ -1,4 +1,5 @@
 import colors
+import random
 import pygame
 pygame.init()
 
@@ -7,11 +8,12 @@ screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 screen.fill((255, 255, 255))
 
-color = colors.different_colors_()
 width = 100
 height = 75
 
 rects = []
+
+running = True
 
 rects.append(pygame.Rect(0, 0, width, height))
 
@@ -27,15 +29,20 @@ rects[-1].bottomright = (screen_width, screen_height)
 rects.append(pygame.Rect(0, 0, width, height))
 rects[-1].center = (screen_width//2, screen_height//2)
 
-for rect in rects:
-    pygame.draw.rect(screen, color, rect)
-pygame.display.flip()
-
-running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-pygame.quit()
 
+    wallpaper = colors.different_colors_()
+    screen.fill(wallpaper)
+
+    for rect in rects:
+        color = colors.different_colors_()
+        pygame.draw.rect(screen, color, rect)
+
+    pygame.display.flip()
+    pygame.time.delay(random.randint(500, 700))
+
+pygame.quit()
